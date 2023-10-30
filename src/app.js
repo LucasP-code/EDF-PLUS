4b892d626c77a3ac8b3438382f75c5dd683f7a45
Lennon Machado da Silva <100393288+13bw@users.noreply.github.com>
2023-10-30 09:07:04 -0300

    feat: Cors adicionado

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const routerAluno = require('./Routers/routerAluno');
 const routerEscola = require('./Routers/routerEscola');
+const cors = require('cors');
 
 app.use(express.json());
-
+app.use(cors())
 app.use(routerAluno);
 app.use(routerEscola);
 
