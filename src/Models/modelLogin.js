a9524a86467fbc2dcf1ba9c7c620624a54667d16
Felslender <felipecavalcantedev1112005@gmail.com>
2023-10-31 09:40:14 -0300

    organização do model e controller

--- src/Models/modelLogin.js
+++ src/Models/modelLogin.js
@@ -1,51 +1,32 @@
 const connection = require('./connection');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 
-const userLogin = async (Login) => {
-  const { Email, Senha } = Login;
 
-  const queryEmail = 'SELECT Email FROM (SELECT Email FROM Alunos UNION SELECT Email_EDF FROM Facilitador UNION SELECT Email FROM Admins) AS Login_Usuario WHERE Email = ?;'
-
-  const [userEmail] = await connection.execute(queryEmail, [Email]);
-
-  if (userEmail.length === 0) {
-    return { msg: "Email não encontrado" };
-    
-  }
-
-  const queryPassword = 'SELECT Senha FROM (SELECT Senha, Email FROM Alunos UNION SELECT Senha, Email_EDF FROM Facilitador UNION SELECT Senha, Email FROM Admins) AS Login_Senha WHERE Email = ?';
+const userLogin = async (Email) => {
+  const queryEmail =
+    'SELECT Email FROM (SELECT Email FROM Alunos UNION SELECT Email_EDF FROM Facilitador UNION SELECT Email FROM Admins) AS Login_Usuario WHERE Email = ?;';
 
   try {
+    const [userEmail] = await connection.execute(queryEmail, [Email]);
+
+    if (userEmail.length === 0) {
+      return { msg: "Email não encontrado" };
+    }
+
+    const queryPassword =
+      'SELECT Senha FROM (SELECT Senha, Email FROM Alunos UNION SELECT Senha, Email_EDF FROM Facilitador UNION SELECT Senha, Email FROM Admins) AS Login_Senha WHERE Email = ?';
+
     const [UserPassword] = await connection.execute(queryPassword, [Email]);
 
     if (UserPassword.length === 0) {
       return { msg: "Usuário não encontrado!" };
     }
 
-    const senhaCorreta = UserPassword[0].Senha;
-
-    const bcryptRes = await new Promise((resolve, reject) => {
-      bcrypt.compare(Senha, senhaCorreta, (bcryptErr, result) => {
-        if (bcryptErr) {
-          reject(bcryptErr);
-        }
-        resolve(result);
-      });
-    });
-
-    if (!bcryptRes) {
-      return { msg: "Email ou senha incorretos!" };
-    }
-
-    const token = jwt.sign({ email: Email }, process.env.SECRET, { expiresIn: '1h' });
-
-    return { token };
+    return UserPassword;
   } catch (error) {
-    console.error(error);
-    return { msg: "Ocorreu um erro durante a autenticação." };
+    return { msg: "Ocorreu um erro ao processar a solicitação" };
   }
 };
+
 
 module.exports = {
 userLogin,
--- src/controllers/controllerLogin.js
+++ src/controllers/controllerLogin.js
@@ -1,11 +1,42 @@
 const models = require('../Models/modelLogin');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const userLogin = async (req, res) => {
+    const { Email, Senha } = req.body;
 
-const userLogin = async(req, res) => {
+    try {
+        const userPasswordResult = await models.userLogin(Email);
+        
+        if (userPasswordResult.msg) {
+            return res.status(401).json({ error: userPasswordResult.msg });
+        }
 
-    const login = await models.userLogin(req.body);
-    return res.status(200).json(login);
+        const senhaCorreta = userPasswordResult[0].Senha;
+
+        const bcryptRes = await new Promise((resolve, reject) => {
+            bcrypt.compare(Senha, senhaCorreta, (bcryptErr, result) => {
+                if (bcryptErr) {
+                    reject(bcryptErr);
+                }
+                resolve(result);
+            });
+        });
+
+        if (!bcryptRes) {
+            return res.status(401).json({ error: "Email ou senha incorretos!" });
+        }
+
+        const token = jwt.sign({ email: Email }, process.env.SECRET, { expiresIn: '1h' });
+
+        return res.status(200).json({ token });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Ocorreu um erro durante a autenticação." });
+    }
 };
+
+  
 
 module.exports = {
 
