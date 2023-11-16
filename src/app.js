4b686d37aada72582f21ed01a153b7e3e97f1440
Lennon Machado da Silva <100393288+13bw@users.noreply.github.com>
2023-11-16 10:33:08 -0300

    fix: try catch removido do createCrianca

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const app = express();
 const cors = require('cors');
 
 
-const routerAluno = require('./routers/routerAluno');
-const routerLogin = require('./routers/routerLogin');
+const routerAluno = require('./Routers/routerAluno');
+const routerLogin = require('./Routers/routerLogin');
 
 app.use(express.json());
 app.use(cors());
